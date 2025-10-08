d7cdd9c1b401e95fade70e7eb720b67fbf893299
Mohammad Faiz Bin Marzuki <faizz.marzukii@gmail.com>
2025-10-08 17:08:51 +0800

    feat: change merchant name

--- src/app/components/merchant-account.tsx
+++ src/app/components/merchant-account.tsx
@@ -8,11 +8,11 @@ interface MerchantAccountProps {
 export default function MerchantAccount({ onClose }: MerchantAccountProps) {
   return (
     <div className="flex items-center gap-2 justify-between border-b border-neutral-200 py-4 px-6 md:border-0">
-            <div className="flex items-center gap-2">
-                <Avatar />
-                <span>John Doe</span>
-            </div>
-            {onClose && <CloseButton onClick={onClose} />}
-        </div>);
+      <div className="flex items-center gap-2">
+        <Avatar />
+        <span className="font-semibold">Joy and Supply Store</span>
+      </div>
+      {onClose && <CloseButton onClick={onClose} />}
+    </div>);
 
 }
